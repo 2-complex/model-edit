5742b8d236e7eb9a5fad4501840310fc22220ff1
Peterson Trethewey <peterson@2-complex.com>
2015-03-08 22:47:05 -0700

    Relinked w2ui submodule to 2-complex fork so I could replace the +/- with awesome-font carets

--- sidebar.js
+++ sidebar.js
@@ -8,19 +8,30 @@ function endsWith(str, suffix)
 function replace_filename(oldpath)
 {
     var newname = document.getElementById('change_file_name_input').value;
-    w2ui['sidebar'].find({id:oldpath})[0].text = newname;
-    w2ui['sidebar'].refresh();
+    var components = oldpath.split('/');
+    var parent = components.slice(0, components.length-1).join('/');
+    w2ui['sidebar'].set(oldpath, {id: parent + '/' + newname, text : newname});
 
     FileManager.rename(oldpath, newname);
 }
 
-function newEditNodeHtml(text, path)
+function make_new_folder(oldpath)
 {
-    return "<input"
+    var newname = document.getElementById('change_file_name_input').value;
+    w2ui['sidebar'].set(oldpath, {text : newname});
+
+    // FileManager.mkdir(oldpath, newname);
+}
+
+function newEditNodeHtml(text, path, function_name)
+{
+    var replacer_text = "<input"
         + " id=\"change_file_name_input\" value=\"" + text + "\" "
-        + " onblur=\"replace_filename('" + path + "');\" "
-        + " onkeydown=\"if (event.keyCode == 13) {replace_filename('" + path + "');}\" "
+        + " onblur=\"" + function_name + "('" + path + "');\" "
+        + " onkeydown=\"if (event.keyCode == 13) {" + function_name + "('" + path + "');}\" "
         + " >";
+
+    w2ui['sidebar'].set(path, {text : replacer_text});
 }
 
 function install_sidebar(lsObject)
@@ -33,9 +44,10 @@ function install_sidebar(lsObject)
             style : 'border: 1px solid silver',
             routeData : { id: 59, vid: '23.323.4' },
             menu: [
-                { id: 1, text: 'Rename', icon: 'fa fa-pencil fa-fw' },
-                { id: 2, text: 'New Folder', icon: 'fa fa-plus fa-fw' },
-                { id: 3, text: 'Delete', icon: 'fa-li fa fa-minus-square' }
+                { text: 'Rename', icon: 'fa fa-pencil fa-fw' },
+                { text: 'New Folder', icon: 'fa fa-plus fa-fw' },
+                { text: 'New Model', icon: 'fa fa-plus fa-fw' },
+                { text: 'Delete', icon: 'fa fa-trash-o' }
             ],
             onMenuClick: function( e )
             {
@@ -49,8 +61,7 @@ function install_sidebar(lsObject)
                 {
                     var node = w2ui['sidebar'].find({id:path})[0];
                     var text = node.text;
-                    node.text = newEditNodeHtml(text, path);
-                    w2ui['sidebar'].refresh();
+                    newEditNodeHtml(text, path, "replace_filename");
                     document.getElementById('change_file_name_input').focus();
                 }
                 else if ( e.menuItem.text == "New Folder" )
@@ -58,9 +69,7 @@ function install_sidebar(lsObject)
                     var node = w2ui['sidebar'].add("workspace",
                         { id: "workspace/newfolder", text: "newfolder", type: "directory", icon: "fa fa-folder-o fa-fw" } );
 
-                    node.text = newEditNodeHtml("newfolder", "workspace/newfolder");
-
-                    w2ui['sidebar'].refresh();
+                    newEditNodeHtml("newfolder", "workspace/newfolder", "make_new_folder");
                     document.getElementById('change_file_name_input').focus();
                 }
             },
